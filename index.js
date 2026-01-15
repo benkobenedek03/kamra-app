e70d7444c856ea13a22f76fd97d6aaa4ac085e96
Benedek Benkó <beni200306@gmail.com>
2026-01-15 10:57:35 +0100

    post, get working

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 const express=require("express")
 const app = express()
 const path = require("path")
+const fs = require("fs/promises")
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname,"public")))
 
 
+
+const getData = async ()=>{
+    return await fs.readFile(path.join(__dirname,"data","products.json"),"utf-8")
+}
+const saveData = async (products)=>{
+    await fs.writeFile(path.join(__dirname,"data","products.json"),JSON.stringify(products))
+}
+
 app.get("/", (req,res)=> {
-    res.json({helath :"ok"})
+    res.send()
 })
 
-app.get("/items", (req,res)=>{
-    res.json({items:"page"})
+app.get("/items",async (req,res)=>{
+
+    const read= await getData()
+    const data=JSON.parse(read)
+    
+    res.json(data)
+
 })
 
 app.get("/items/:id", (req,res)=>{
     res.json({id:req.params.id})
 })
 
-app.post("/items", (req,res) =>{
-    
+app.post("/items", async (req,res) =>{
+    let data= JSON.parse(await getData())
+
     const body ={
         name:req.body.name,
         quantity:req.body.quantity,
-        unit:req.body.unit,
-        category:req.body.category,
-        uploaded:req.body.uploaded
+        unit:req.body.unit
     } 
-    
+    data.push(body)
 
-    res.json()
+    await saveData(data)
+    
+    
+    res.json({status:"ok"})
 })
 
 
--- public/script.js
+++ public/script.js
@@ -1,1 +1,58 @@
-asdasdasd
+async function getData(){
+    
+    try {
+        console.log("asd")
+        const response = await fetch('/items');
+        const items = await response.json();
+        console.log(items);
+        loadData(items)
+        return items;
+
+    } catch (error) {
+        alert("nem sikerült betölteni az oldalt")
+    }
+
+}
+
+async function loadData(items)
+{
+     //const data = await getData()
+     const list = document.querySelector('#product-list')
+     list.innerHTML="";
+
+     items.forEach(element => {
+        const li = document.createElement('li')
+        li.innerHTML=
+        li.innerHTML = `${element.name} - ${element.quantity} ${element.unit}`
+        list.appendChild(li)
+        
+     });
+}
+
+async function saveProduct()
+{
+    const name=document.querySelector('#n').value
+    const quantity=document.querySelector('#q').value
+    const unit=document.querySelector('#u').value
+
+    console.log({
+        name:name,
+        quantity:quantity,
+        unit:unit
+    })
+    
+
+    const response= await fetch("/items", {
+        method:"POST",
+        body: JSON.stringify({
+        name:name,
+        quantity:quantity,
+        unit:unit}),
+        headers:{
+             "Content-Type": "application/json",
+        }
+    })
+    console.log(await response.json())
+    await loadData(await getData()) 
+}
+getData()
